f03af242087f31b183b156595d8a914281dc4ea8
Muskan4556 <muskan35163@gmail.com>
2024-08-18 09:16:24 +0530

    Variables

---
+++ variable/Employee.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class Employee {
+    int empId;
+    public void setId(int id){
+        this.empId = id;
+    }
+    public int getId(){
+        return this.empId;
+    }
+
+    
+}
---
+++ variable/Engineer.java
@@ -0,0 +1,8 @@
+package variable;
+
+public class Engineer implements Person {
+    @Override
+    public String profession(){
+        return "Software Engineer";
+    }
+}
