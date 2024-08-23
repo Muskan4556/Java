cef1feec34b88efcf5b455f926a7d217ebf352e7
Muskan4556 <muskan35163@gmail.com>
2024-08-24 01:47:27 +0530

    fix file structure

---
+++ Person.java
@@ -0,0 +1,12 @@
+
+public class Person {
+    int id;
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.id = 1;
+        System.out.println(person.id);
+    }
+}
+
+// classes we create using "new" keyword
