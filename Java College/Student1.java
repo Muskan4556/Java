8b6e65421f238b74d3a68c000e7b0c8003cb3feb
Muskan4556 <muskan35163@gmail.com>
2024-08-23 10:29:14 +0530

    add Student class

---
+++ Java College/Student1.java
@@ -0,0 +1,73 @@
+public class Student1 {
+    private long id;
+    private String name;
+    private double marks1;
+    private double marks2;
+    private double marks3;
+
+    Student1(){
+        this.id = 0;
+        this.name = "";
+        this.marks1 = 0.0;
+        this.marks2 = 0.0;
+        this.marks3 = 0.0;
+    }
+
+    Student1(long id, String name, double m1,double m2, double m3){
+        this.id = id;
+        this.name = name;
+        this.marks1 = m1;
+        this.marks2 = m2;
+        this.marks3 = m3;
+    }
+
+    public double totalMarks(){
+        return this.marks1 + this.marks2 + this.marks3;
+    }
+
+    public void setId(long id){
+        this.id = id;
+    }
+
+    public long getId(){return this.id;}
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setMarks1(double m1){
+        this.marks1 = m1;
+    }
+    public void setMarks2(double m2){
+        this.marks2 = m2;
+    }
+    public void setMarks3(double m2){
+        this.marks2 = m2;
+    }
+
+    public double getMarks1(){return this.marks1;}
+    public double getMarks2(){return this.marks2;}
+    public double getMarks3(){return this.marks3;}
+
+    public String toString(){
+        return (
+            "ID: " + this.id + "\n" +
+            "Name: " + this.name + "\n" +
+            "Marks1: " + this.marks1 + "\n" +
+            "Marks2: " + this.marks2 + "\n" +
+            "Marks3: " + this.marks3 + "\n"
+        );
+    }
+
+    public static void main(String[] args) {
+        // Student1 s1 = new Student1();
+        Student1 s1 = new Student1(1234567890,"abc",77.57,45.25,98.48);
+        System.out.println(s1.toString());
+        System.out.println(s1.totalMarks());
+
+    }
+}
