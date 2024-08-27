4f4a933695de2f18a096d59fe1ff9f4f5612d163
Muskan4556 <muskan35163@gmail.com>
2024-08-27 12:27:42 +0530

    Overloading + Variable number of args method

---
+++ Java2/Overloading.java
@@ -0,0 +1,49 @@
+package Java2;
+
+public class Overloading {
+    
+    static void sum(){
+        System.out.println("method_1");
+    }  
+
+    static void sum(int a){
+        System.out.println("method_2 and value of a is: " + a);
+    }
+    
+    static void sum(double a){
+        System.out.println("method_3 and value of a is: " + a);
+    }  
+
+    static void sum(String a){
+        System.out.println("method_4 and value of a is: " + a);
+    } 
+    
+    static void sum(int a, double b){
+        System.out.println("method_5 and value of a is: " + a);
+    } 
+
+    static void sum(double a, int b){
+        System.out.println("method_6 and value of a is: " + a);
+    } 
+
+    // Variable number of args method
+    public double sum (double ...a){
+        double sum = 0.0;
+        for(int i=0; i<a.length;i++){
+            sum += a[i];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Overloading.sum();
+        Overloading.sum(0);
+        Overloading.sum(0.0);
+        Overloading.sum("Hello");
+        Overloading.sum(0, 0.0);
+        Overloading.sum(0.0, 0);
+
+        Overloading obj = new Overloading();
+        System.out.println(obj.sum(50.2,60.4,70.8));
+    }
+}
