f86f5efe214fbd28af6f0f7fd4c5e9ac7749fc10
Muskan4556 <muskan35163@gmail.com>
2024-08-27 12:21:38 +0530

    add Calculator Class

---
+++ Java2/Calculator.java
@@ -0,0 +1,10 @@
+package Java2;
+
+public class Calculator {
+    public static int sum(int a,int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+       System.out.println( Calculator.sum(10, 00));
+    }
+}
